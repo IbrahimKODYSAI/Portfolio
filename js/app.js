c691bd9c1b97730c2f9889c2d22c115de095233c
IbrahimKODYSAI <48011040+IbrahimKODYSAI@users.noreply.github.com>
2020-03-24 13:12:18 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,65 @@
+const datas = [
+  "Infographiste débutant",
+  "Photographe amateur",
+  "Webdisigner débutant"
+]
+const project = [
+  {
+    id: 1,
+    category: 'Angular',
+    title: 'Angular, une fausse bonne idée ?',
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    id: 2,
+    category: 'React',
+    title: 'React, une vraie bonne idée ?',
+    excerpt: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    id: 3,
+    category: 'O’clock',
+    title: 'O’clock, une vraie bonne école ?',
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    id: 4,
+    category: 'Autre',
+    title: 'Pourquoi a-t-on besoin de développeurs ?',
+    excerpt: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    id: 5,
+    category: 'React',
+    title: 'React-Native, la solution mobile ?',
+    excerpt: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    id: 6,
+    category: 'Autre',
+    title: 'Dis donc, que des questions ?',
+    excerpt: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet. consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+];
+
+const app = {
+  init : function(){
+    app.switchTxt = document.getElementById('switch');
+    app.h1 = document.getElementById('fadeout');
+    app.getRandomTxt();
+    app.displayRandomTxt();
+  },
+
+  getRandomTxt : function (){
+    var randomTxt = datas[Math.floor(Math.random()*datas.length)];
+    // console.log(randomTxt)
+    app.h1.textContent = randomTxt;
+  },
+
+  displayRandomTxt : function(){
+    var setimer = setInterval(app.getRandomTxt, 2000);
+  }
+
+}
+
+document.addEventListener('DOMContentLoaded', app.init);
